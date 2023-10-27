c91da93be25b42fc6fb6ef26979d9a7f15b835b4
Joseph Barnett <joseph-barnett@outlook.com>
2023-10-27 16:34:28 +0100

    implemented while loop

--- RPS.js
+++ RPS.js
@@ -1,53 +1,48 @@
-"use strict";
-
 const ps = require("prompt-sync");
 const prompt = ps({sigint: true});
-
-let inp = prompt("please enter, rock, paper or scissors : ");
-
 
     let scoreYou = 0;
     let scoreComputer = 0;
 
+function play() {
+
+    let game = true;
+
+    while (game) { 
+
+        const you = Number(prompt("pick 1 for rock, 2 for paper or 3 for scissors: "));
+
+        const comp = (Math.floor(Math.random()*3) +1);
+
+        console.log(`you ${you}`)
+
+        console.log(`comp ${comp}`)
+}
+}
 
 function rockPaperScissors(a, b) {
 
     if (a === scissors && b === paper) {
-        return "You win" && scoreYou + 1;
+        return "You win" && scoreYou + 1;            // scissors v paper
 
     } else if (a === paper && b === scissors) {
-        return "Computer wins" && scoreComputer + 1;
+        return "Computer wins" && scoreComputer + 1; // paper v scissors
 
     } else if (a === scissors && b === rock) {
-        return "Computer wins" && scoreComputer + 1;
+        return "Computer wins" && scoreComputer + 1; // scissors v rock
 
     } else if (a === rock && b === scissors) {
-        return "You win" && scoreYou + 1;
+        return "You win" && scoreYou + 1;            // rock v scissors
 
     } else if (a === paper && b === rock) {
-        return "You win" && scoreYou + 1;
+        return "You win" && scoreYou + 1;            // paper v rock
 
     } else if (a === rock && b === paper) {
-        return "Computer wins" && scoreComputer + 1;
+        return "Computer wins" && scoreComputer + 1; // rock v paper
 
     } else if (a === b) {
         return "draw";
     }
 }
 
-function computerTurn() {
-
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch (randNum) {
-        case 1:
-            computer = "ROCK";
-            break;
-        case 2:
-            computer = "PAPER";
-            break;
-        case 3:
-            computer = "SCISSORS";
-            break;
-    }
-}
+play ()
--- RPS2.js
+++
@@ -1,1 +0,0 @@
-const { rockPaperScissors } = require("./RPS");
