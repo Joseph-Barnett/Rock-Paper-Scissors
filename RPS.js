9f5150400a799325f8bd217231461690095d58cd
Joseph Barnett <joseph-barnett@outlook.com>
2023-10-27 16:01:04 +0100

    added computer variable

--- RPS.js
+++ RPS.js
@@ -1,29 +1,53 @@
+"use strict";
+
+const ps = require("prompt-sync");
+const prompt = ps({sigint: true});
+
+let inp = prompt("please enter, rock, paper or scissors : ");
+
+
+    let scoreYou = 0;
+    let scoreComputer = 0;
+
+
 function rockPaperScissors(a, b) {
-    
-    let scoreA = 0;
-    let scoreB = 0; 
-    
-    
+
     if (a === scissors && b === paper) {
-        return "a wins" && scoreA + 1;
+        return "You win" && scoreYou + 1;
 
-    }   else if (a === paper && b === scissors) {
-        return "b wins" && scoreB + 1;
+    } else if (a === paper && b === scissors) {
+        return "Computer wins" && scoreComputer + 1;
 
-    }   else if (a === scissors && b === rock) {
-        return "b wins" && scoreB + 1;
+    } else if (a === scissors && b === rock) {
+        return "Computer wins" && scoreComputer + 1;
 
-    }   else if (a === rock && b === scissors) {
-        return "a wins" && scoreA + 1;
+    } else if (a === rock && b === scissors) {
+        return "You win" && scoreYou + 1;
 
-    }   else if (a === paper && b === rock) {
-        return "a wins" && scoreA + 1;
+    } else if (a === paper && b === rock) {
+        return "You win" && scoreYou + 1;
 
-    }   else if (a === rock && b === paper) {
-        return "b wins" && scoreB + 1;
+    } else if (a === rock && b === paper) {
+        return "Computer wins" && scoreComputer + 1;
 
-    }   else if (a === b) {
+    } else if (a === b) {
         return "draw";
-    }   
+    }
 }
 
+function computerTurn() {
+
+    const randNum = Math.floor(Math.random() * 3) + 1;
+
+    switch (randNum) {
+        case 1:
+            computer = "ROCK";
+            break;
+        case 2:
+            computer = "PAPER";
+            break;
+        case 3:
+            computer = "SCISSORS";
+            break;
+    }
+}
---
+++ RPS2.js
@@ -0,0 +1,1 @@
+const { rockPaperScissors } = require("./RPS");
