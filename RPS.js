2559ec014ceb0a7d52b2b0e977f930d5c9ce86a7
Joseph Barnett <joseph-barnett@outlook.com>
2023-10-27 14:50:32 +0100

    Added scores

--- RPS.js
+++ RPS.js
@@ -1,31 +1,29 @@
 function rockPaperScissors(a, b) {
+    
+    let scoreA = 0;
+    let scoreB = 0; 
+    
+    
     if (a === scissors && b === paper) {
-        return "a wins";
+        return "a wins" && scoreA + 1;
 
     }   else if (a === paper && b === scissors) {
-        return "b wins";
+        return "b wins" && scoreB + 1;
 
     }   else if (a === scissors && b === rock) {
-        return "b wins";
+        return "b wins" && scoreB + 1;
 
     }   else if (a === rock && b === scissors) {
-        return "a wins";
+        return "a wins" && scoreA + 1;
 
     }   else if (a === paper && b === rock) {
-        return "a wins";
+        return "a wins" && scoreA + 1;
 
     }   else if (a === rock && b === paper) {
-        return "b wins";
+        return "b wins" && scoreB + 1;
 
-    }   else if (a === b) {
-        return "draw";
-    
-    }   else if (a === b) {
-        return "draw";
-    
     }   else if (a === b) {
         return "draw";
     }   
-
-
 }
+
